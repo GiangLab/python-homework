c4d597de3375adb7bddc43ffc5e91e455f011485
Ващилко Алексей <aleksejvv17@gmail.com>
2025-01-18 23:19:55 +0100

    Update config.py

--- translatorBot/config.py
+++ translatorBot/config.py
@@ -1,4 +1,4 @@
 #t.me/recognize_translate_bot
-TOKEN = "8037016484:AAECsER1zanc1cWp6r8U7DZRiXOslKnmoxo"
+TOKEN = "YOUR TOKEN"
 PATH_Tesseract =r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 PATH_img =r"imgText.jpg"
